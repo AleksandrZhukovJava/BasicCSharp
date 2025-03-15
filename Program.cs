bf09370a8c396d109afa2bffa959fc26019b5733
Александр Жуков <a.zhukov@uzum.com>
2025-03-15 11:15:05 +0100

    homework variables done

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BasicCSharp
 {
@@ -10,6 +11,16 @@ namespace BasicCSharp
     {
         static void Main(string[] args)
         {
+            byte firstVariableWithoutMeaning = 127;
+            short secondVariableWithoutMeaning = short.MaxValue;
+            int thirdVariableWithoutMeaning = -2147483648;
+            long fourthVariableWithoutMeaning = long.MinValue;
+            float fifthVariableWithoutMeaning = 0.0f;
+            double sixthVariableWithoutMeaning = 0.0;
+            bool seventhVariableWithoutMeaning = Convert.ToBoolean(1);
+            uint eighthVariableWithoutMeaning = 4294967295;
+            String tenthVariableExplanation = "Пробую класс, просто для теста";
+            Program programmInstance = new Program();
         }
     }
 }
